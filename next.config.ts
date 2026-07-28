333b8fb6932b5738cb920127d44c2ba5340e4fe1
Maretyui <129848669+Maretyui@users.noreply.github.com>
2026-07-28 04:03:50 +0000

    Add temp.maretyui.com to allowedDevOrigins for dev-preview support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["temp.maretyui.com"],
 };
 
 export default nextConfig;
